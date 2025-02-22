98754f6bb05e2120a7ab373f1050b6a5a88dc712
Daniel Alves <danielallves903@gmail.com>
2025-02-22 18:26:35 -0300

    começando a por o backend

--- public/js/script2.js
+++ public/js/script2.js
@@ -23,23 +23,68 @@ let meses = [
   "Dezembro",
 ];
 
-for(let m = 0 ; m < meses.length; m++){
-let option_mes = document.createElement("option");
-option_mes.value = meses[m];
-option_mes.textContent = meses[m]
-mes.appendChild(option_mes)
+for (let m = 0; m < meses.length; m++) {
+  let option_mes = document.createElement("option");
+  option_mes.value = meses[m];
+  option_mes.textContent = meses[m];
+  mes.appendChild(option_mes);
 }
 
-let ano = document.querySelector("#ano")
+let ano = document.querySelector("#ano");
 
-for(let a = 2020 ; a >= 1926 ; a--){
-    let option_ano = document.createElement("option");
-    option_ano.value = a;
-    option_ano.textContent = a;
-    ano.appendChild(option_ano)
+for (let a = 2020; a >= 1926; a--) {
+  let option_ano = document.createElement("option");
+  option_ano.value = a;
+  option_ano.textContent = a;
+  ano.appendChild(option_ano);
 }
 
 let volta = document.querySelector("#close");
-volta.addEventListener("click",()=>{
- window.location.href = "http://127.0.0.1:3000/"
-})
+volta.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:3000/";
+});
+
+function pegar_dados() {
+  let dia_ani = dia.value;
+  let mes_ani = mes.value;
+  let ano_ani = ano.value;
+
+  if (dia_ani !== "Dia" && mes_ani !== "Mês" && ano_ani !== "Ano") {
+    const dataNascimento = `${dia_ani}/${mes_ani}/${ano_ani}`;
+
+    const dados = {
+      nascimento: dataNascimento,
+      usuario: document.querySelector("#usuario").value,
+      senha: document.querySelector("#senha").value,
+    };
+
+    enviar_dados(dados);
+  } else {
+    alert("Poer favor preecha os dados corretamente");
+  }
+}
+
+function enviar_dados(dados) {
+  fetch("http://127.0.0.1:3000/usuarios", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dados),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log("Dados enviados: ", data);
+    })
+    .catch((erro) => {
+      console.log(erro);
+    });
+}
+document.querySelector("#enviar").addEventListener("click", (evento) => {
+  evento.preventDefault();
+
+  pegar_dados();
+});
--- serve.js
+++ serve.js
@@ -1,11 +1,17 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+app.use(cors());
+
+const usuarios = []
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -15,9 +21,21 @@ app.get("/sign", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index2.html"));
 });
 
-app.post("/sign",(req,res)=>{
-  console.log(req.body)
-})
+app.post("/sign", (req, res) => {
+  console.log(req.body);
+});
+
+app.get("/usuarios", (req, res) => {
+  res.send(JSON.stringify(usuarios));
+});
+
+app.post("/usuarios", (req, res) => {
+  const dados = req.body;
+  usuarios.push(dados)
+  console.log("Dados recebidos: ", dados);
+
+  res.status(200).json({ message: "Dados recebidos com secusso",dados });
+});
 
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index3.html"));
